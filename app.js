478a8db4b0068bcecbdb6a2023ef91c169fc9aad
Roger Davenport <roger.neil.davenport@gmail.com>
2017-10-23 18:26:54 -0700

    did a bunch of stuff im not sure I understand

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ function getRandomIntInclusive(min, max) {
 }
 
 
-
-
+// this is the first store object
 var storeNum1  = {
   storename: '1st & Pike',
   minCust: 23,
@@ -18,14 +17,15 @@ var storeNum1  = {
   },
   aveHourlySales: function () {
     var salesByHour = [];
-    for (var i = 0; i < 14; i++)  {
+    for (var i = 0; i < 15; i++) {
       salesByHour.push(Math.ceil(this.hourlyCust() * this.aveSales));
     }
     return salesByHour;
   }
 
 };
-var storenum2  = {
+// this is the second store
+var storeNum2  = {
   storename: 'Seatac Airport',
   minCust: 3,
   maxCust: 23,
@@ -35,14 +35,14 @@ var storenum2  = {
   },
   aveHourlySales: function () {
     var salesByHour = [];
-    for (var i = 0; i < 14; i++  {
+    for (var i = 0; i < 15; i++) {
       salesByHour.push(Math.ceil(this.hourlyCust() * this.aveSales));
     }
     return salesByHour;
   }
 };
-
-var storenum3  = {
+// this is the 3rd store
+var storeNum3 = {
   storename: 'Seattle Center',
   minCust: 11,
   maxCust: 38,
@@ -52,14 +52,14 @@ var storenum3  = {
   },
   aveHourlySales: function () {
     var salesByHour = [];
-    for (var i = 0; i < 14; i++) {
+    for (var i = 0; i < 15; i++) {
       salesByHour.push(Math.ceil(this.hourlyCust() * this.aveSales));
     }
     return salesByHour;
   }
 };
-
-var storenum4  = {
+// this is the 4th store
+var storeNum4  = {
   storename: 'Capitol Hill',
   minCust: 20,
   maxCust: 35,
@@ -69,14 +69,14 @@ var storenum4  = {
   },
   aveHourlySales: function () {
     var salesByHour = [];
-    for (var i = 0; i < 14; i++) {
+    for (var i = 0; i < 15; i++) {
       salesByHour.push(Math.ceil(this.hourlyCust() * this.aveSales));
     }
     return salesByHour;
   }
 };
-
-var storenum5  = {
+//this is the 5th store
+var storeNum5  = {
   storename: 'Alki',
   minCust: 2,
   maxCust: 16,
@@ -86,9 +86,48 @@ var storenum5  = {
   },
   aveHourlySales: function () {
     var salesByHour = [];
-    for (var i = 0; i < 14; i++) {
+    for (var i = 0; i < 15; i++) {
       salesByHour.push(Math.ceil(this.hourlyCust() * this.aveSales));
     }
     return salesByHour;
   }
 };
+
+//array with sales hours
+var openHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+
+function putListinDoc(storeLocation) {
+//listhead creates a heading with store name for each store
+  var listhead = document.createElement('div');
+  listhead.innerHTML = '<p>' + storeLocation.storename + '</p>';
+  //document.body.appendChild(listhead);
+
+  var hoursList = [];
+  var x = storeLocation.aveHourlySales(); // creates array with hoursly sales
+  console.log(x);
+  //creates array of 'hours ' + values
+  for (var j = 0; j < openHours.length; j++) {
+    hoursList.push(openHours[j] + '  ' + x[j]);
+  }
+  console.log(hoursList);
+  // creates li elements for each open hour with sales data
+  var ulelement = document.createElement('ul');
+
+  var lines = '';
+
+  for ( var k = 0; k < hoursList.length; k++) {
+    lines = lines + '<li>' + hoursList[k] + '</li>';
+  }
+  //for ( var m = 0; m < liArray.length; m++) {
+  //  ulelement.appendChild(liArray[m]);
+  //}
+  ulelement.innerHTML = lines;
+  listhead.appendChild(ulelement);
+  document.body.appendChild(listhead);
+}
+
+putListinDoc(storeNum1);
+putListinDoc(storeNum2);
+putListinDoc(storeNum3);
+putListinDoc(storeNum4);
+putListinDoc(storeNum5);
